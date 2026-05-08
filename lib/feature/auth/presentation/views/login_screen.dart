e9c26c956977a8fe7f56a59881de72e8708a269e
VaradSinghal <varadsinghal1526@gmail.com>
2026-05-08 18:12:25 +0530

    login screen

--- lib/feature/auth/presentation/views/login_screen.dart
+++ lib/feature/auth/presentation/views/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,8 +7,48 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Spacer(),
+            Image.asset("assets/images/ig_logo.png", width: 60, height: 60),
+
+            const SizedBox(height: 80),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  _inputField('Username, email or phone number'),
+                  SizedBox(height: 12),
+                  _inputField('Password', isPassword: true),
+                  SizedBox(height: 18),
+                ],
+              ),
+            ),
+
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputField(String hint, {bool isPassword = false}) {
+    return Container(
+      height: 48,
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white.withValues(alpha: 0.2)),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TextField(
+        obscureText: isPassword,
+        style: GoogleFonts.outfit(color: Colors.white, fontSize: 14),
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: GoogleFonts.outfit(color: Colors.grey, fontSize: 14),
+          border: InputBorder.none,
+        ),
       ),
     );
   }
