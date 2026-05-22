ee785ada17acb794327b6bc69b7dab7753a7d0c3
VaradSinghal <varadsinghal1526@gmail.com>
2026-05-22 14:03:05 +0530

    termsandconditions

--- lib/feature/auth/presentation/views/terms_screen.dart
+++ lib/feature/auth/presentation/views/terms_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:instagram/core/common/widgets/ig_button.dart';
+import 'package:instagram/feature/auth/presentation/widgets/already_have_account_section.dart';
 
 class TermsScreen extends StatelessWidget {
   const TermsScreen({super.key});
@@ -13,14 +16,63 @@ class TermsScreen extends StatelessWidget {
           icon: Icon(Icons.arrow_back_ios, color: Colors.white),
         ),
       ),
-      body: Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          Text(
-            "Agree to Instagram's terms and policies"
-          ),
-        ],
-      ),),
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              "Agree to Instagram's terms and policies",
+              style: GoogleFonts.outfit(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            RichText(
+              text: TextSpan(
+                style: GoogleFonts.outfit(
+                  fontSize: 13,
+                  color: Colors.white,
+                  height: 1.5,
+                ),
+                children: [
+                  TextSpan(
+                    text:
+                        "People who use our service may have uploaded your contact information to Instagram.",
+                  ),
+                  TextSpan(text: "Learn more\n\n"),
+                  TextSpan(
+                    text:
+                        "By tapping I agree, you agree to create an account and to Instagram\ 's",
+                  ),
+                  TextSpan(text: "Terms, Privacy Policy."),
+                  TextSpan(text: "and "),
+                  TextSpan(text: "Cookies Policy.\n\n"),
+                  TextSpan(
+                    text:
+                        "The Privacy Policy describes the ways we can use the information we collect when you create an acccount. For example, we use this information to provice, personalize and imporve our products, including ads.",
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            SizedBox(
+              width: double.infinity,
+              height: 40,
+              child: IGButton(text: 'I agree', onPressed: () {
+                
+              }),
+            ),
+            Spacer(),
+            AlreadyHaveAccountSection(),
+            SizedBox(height: 24),
+          ],
+        ),
+      ),
     );
   }
 }
